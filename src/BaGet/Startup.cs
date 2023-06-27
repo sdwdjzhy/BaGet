338b1302065da955c824d002dd298c1715dee2b6
sdwdjzhy <sdwdjzhy@qq.com>
2023-06-27 09:26:01 +0800

    init NET6.0

--- src/BaGet/Startup.cs
+++ src/BaGet/Startup.cs
@@ -57,21 +57,21 @@ namespace BaGet
         private void ConfigureBaGetApplication(BaGetApplication app)
         {
             // Add database providers.
-            app.AddAzureTableDatabase();
-            app.AddMySqlDatabase();
-            app.AddPostgreSqlDatabase();
+            //app.AddAzureTableDatabase();
+            //app.AddMySqlDatabase();
+            //app.AddPostgreSqlDatabase();
             app.AddSqliteDatabase();
-            app.AddSqlServerDatabase();
+            //app.AddSqlServerDatabase();
 
             // Add storage providers.
             app.AddFileStorage();
-            app.AddAliyunOssStorage();
-            app.AddAwsS3Storage();
-            app.AddAzureBlobStorage();
-            app.AddGoogleCloudStorage();
+            //app.AddAliyunOssStorage();
+            //app.AddAwsS3Storage();
+            //app.AddAzureBlobStorage();
+            //app.AddGoogleCloudStorage();
 
             // Add search providers.
-            app.AddAzureSearch();
+            //app.AddAzureSearch();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
